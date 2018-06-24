d041175c46fd8fd9c484640c20b3efd8a852a34b
jose <jennymarin1989@hotmail.com>
2018-06-24 15:45:41 +0100

    Add state and event handler in search_bar

--- src/components/Search_bar.js
+++ src/components/Search_bar.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input />
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { 
+      searchTerm: ''
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <input onChange={ event => this.setState({ searchTerm: event.target.value})}/>
+      </div>
+    )
+  }
+
 }
 
 export default SearchBar;
